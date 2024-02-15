32f78de949d0c3ac63efdec6ddfabc1ec87925ba
Rick Ryan <rick.ryan2@outlook.com>
2024-02-15 23:51:41 +0000

    feat: basic install
    * nvim
    * zsh
    * top

--- setup.sh
+++ setup.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 echo "Start setup system..."
 
+echo "Checking updates..."
 sudo apt update
 
-# btop is the better top for linux
+echo "btop is the better top for linux"
 sudo apt install btop
+
+echo "installing zsh"
+sudo apt install zsh
+
+echo "install nvim"
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+export PATH="$PATH:/opt/nvim-linux64/bin"
+
+
