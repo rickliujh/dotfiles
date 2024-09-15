27d621b19baf4d09457e13726f0d7a1a0679d5d2
Rick Ryan <rick.ryan2@outlook.com>
2024-09-15 22:03:54 +0100

    feat(nvim): replace simpler surrounding pulgin

--- config/nvim/lua/plugins/surround.lua
+++ config/nvim/lua/plugins/surround.lua
@@ -1,22 +1,32 @@
 return {
-  "kylechui/nvim-surround",
-  version = "*",   -- Use for stability; omit to use `main` branch for the latest features
-  event = "VeryLazy",
-  config = function()
-    require("nvim-surround").setup({
-      keymaps = {
-        insert = "<C-g>s",
-        insert_line = "<C-g>S",
-        normal = "sa",
-        normal_cur = "sas",
-        normal_line = "Sa",
-        normal_cur_line = "SSa",
-        visual = "S",
-        visual_line = "gS",
-        delete = "sd",
-        change = "sc",
-        change_line = "Sc",
-      },
-    })
-  end
+  'echasnovski/mini.surround',
+  version = '*',
+  opts = {
+    mappings = {
+      add = 'f',          -- Add surrounding in Normal and Visual modes
+      delete = 'fd',       -- Delete surrounding
+      replace = 'fc',      -- Replace surrounding
+    },
+  }
 }
+
+-- return {
+--   "kylechui/nvim-surround",
+--   version = "*",   -- Use for stability; omit to use `main` branch for the latest features
+--   event = "VeryLazy",
+--   config = function()
+--     require("nvim-surround").setup({
+--       keymaps = {
+--         normal = "fs",
+--         normal_cur = "fsc",
+--         normal_line = "fsl",
+--         normal_cur_line = "fscl",
+--         visual = "fs",
+--         visual_line = "fsl",
+--         delete = "fd",
+--         change = "fc",
+--         change_line = "fcl",
+--       },
+--     })
+--   end
+-- }
