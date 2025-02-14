c9bbdf320d0106394e1bfd0518d36ceab3d8a481
Rick Ryan <rick.ryan2@outlook.com>
2025-02-14 17:49:47 +0000

    feat: docker and sysbox-ce installer

--- installer.sh
+++ installer.sh
@@ -123,6 +123,44 @@ install_dev_pkgs() {
     sudo apt-get install -y build-essential
 }
 
+install_docker() {
+    if ! command -v docker 2>&1 >/dev/null
+    then
+        echo "uninstall conflicting packages..."
+        for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+        
+        echo "setting up Docker's apt repository..."
+        # Add Docker's official GPG key:
+        sudo apt-get update
+        sudo apt-get install ca-certificates curl
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+        # Add the repository to Apt sources:
+        echo \
+          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+          $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+          sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt-get update
+
+        echo "install the latest version..."
+        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    fi
+}
+
+install_sysbox() {
+    rm -rf ./sysbox-ce.deb
+    version="https://downloads.nestybox.com/sysbox/releases/v0.6.6/sysbox-ce_0.6.6-0.linux_amd64.deb"
+    curl -L $version -o sysbox-ce.deb
+    docker rm $(docker ps -a -q) -f || true
+    sudo apt-get install jq
+    sudo apt-get install ./sysbox-ce.deb
+    rm -rf ./sysbox-ce.deb
+
+    # to uninstall, see: https://github.com/nestybox/sysbox/blob/master/docs/user-guide/install-package.md#uninstallation
+}
+
 install_extras() {
     log_task "Installing extra package..."
     check_lang_installed
@@ -130,6 +168,8 @@ install_extras() {
     cargo install eza
     cargo install topgrade 
     go install github.com/jesseduffield/lazygit@latest
+    install_docker
+    install_sysbox
 }
 
 install_without_languages() {
