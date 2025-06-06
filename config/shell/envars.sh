c5a7edff7d1eb2bd721308bbeddfa92620351731
rick liu <rick.ryan2@outlook.com>
2025-06-06 17:24:32 +0100

    fix(shell): ssh error unsuitable terminal xterm-ghostty

--- config/shell/envars.sh
+++ config/shell/envars.sh
@@ -6,3 +6,8 @@ export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:$HOME/go/bin
 export PATH=$PATH:$HOME/.cargo/bin
 
+# fix ssh error: Unsuitable Terminal xterm-ghostty
+# https://vninja.net/2024/12/28/ghostty-workaround-for-missing-or-unsuitable-terminal-xterm-ghostty/
+if [[ "$TERM_PROGRAM" == "ghostty" ]]; then
+    export TERM=xterm-256color
+fi
