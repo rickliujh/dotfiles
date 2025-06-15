6ebb635ba51bc8ab698dd877385e19f4e9e63c70
Rick Ryan <rick.ryan2@outlook.com>
2025-06-15 23:46:01 +0100

    feat(config): notify style config

--- config/nvim/lua/plugins/noice-cmd.lua
+++ config/nvim/lua/plugins/noice-cmd.lua
@@ -11,46 +11,57 @@
 --]]
 return {
   {
-    "folke/noice.nvim",
-    event = "VeryLazy",
+    'folke/noice.nvim',
+    event = 'VeryLazy',
     opts = {
       lsp = {
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
         },
       },
       -- you can enable a preset for easier configuration
       presets = {
-        bottom_search = true,         -- use a classic bottom cmdline for search
-        command_palette = false,      -- position the cmdline and popupmenu together
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = false, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = true,        -- add a border to hover docs and signature help
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+      notify = {
+        enabled = true,
+        view = 'notify',
       },
     },
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
+      'MunifTanjim/nui.nvim',
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
+      {
+        'rcarriga/nvim-notify',
+        opts = {
+          background_colour = '#000000',
+          render = 'wrapped-compact',
+          stages = 'static',
+        },
+      },
     },
     config = function(_, opts)
-      local noice = require("noice")
+      local noice = require 'noice'
 
       noice.setup(opts)
 
-      vim.keymap.set("n", "<leader>nc", function()
-        noice.cmd("dismiss")
+      vim.keymap.set('n', '<leader>nc', function()
+        noice.cmd 'dismiss'
       end, { desc = 'Noice: dismiss all visible messages' })
 
-      vim.keymap.set("n", "<leader>nh", function()
-        noice.cmd("pick")
+      vim.keymap.set('n', '<leader>nh', function()
+        noice.cmd 'pick'
       end, { desc = 'Noice: browse notification history' })
-    end
-  }
+    end,
+  },
 }
