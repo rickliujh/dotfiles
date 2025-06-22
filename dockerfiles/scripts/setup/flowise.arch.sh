1b867144fa166bd2ae09860e22f0e56aede4f173
rick liu <rick.ryan2@outlook.com>
2025-06-22 22:58:24 +0100

    feat(dockerfiles): update flowise setup sh

--- dockerfiles/scripts/setup/flowise.arch.sh
+++ dockerfiles/scripts/setup/flowise.arch.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+bash nvm install 20 && nvm use 20
+
 pacman -S npm python python-pip python-setuptools cmake blas lapack
