5d9895022197de648da4bdc46f140387f462ffe1
Rick Ryan <rick.ryan2@outlook.com>
2025-12-16 01:14:53 +0000

    feat(nvim): replace notify with mini

--- config/nvim/lua/plugins/noice-cmd.lua
+++ config/nvim/lua/plugins/noice-cmd.lua
@@ -41,14 +41,14 @@ return {
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      {
-        'rcarriga/nvim-notify',
-        opts = {
-          background_colour = '#000000',
-          render = 'wrapped-compact',
-          stages = 'static',
-        },
-      },
+      -- {
+      --   'rcarriga/nvim-notify',
+      --   opts = {
+      --     background_colour = '#000000',
+      --     render = 'wrapped-compact',
+      --     stages = 'static',
+      --   },
+      -- },
     },
     config = function(_, opts)
       local noice = require 'noice'
