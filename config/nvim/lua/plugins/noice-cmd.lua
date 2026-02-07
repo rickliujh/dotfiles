f2f0cd4ccb34808c32e346a636753f56244f7cac
Rick Ryan <rick.ryan2@outlook.com>
2026-02-07 23:46:46 +0000

    fix(nvim): shell execution result won't display

--- config/nvim/lua/plugins/noice-cmd.lua
+++ config/nvim/lua/plugins/noice-cmd.lua
@@ -26,12 +26,24 @@ return {
       presets = {
         bottom_search = true, -- use a classic bottom cmdline for search
         command_palette = false, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
+        long_message_to_split = false, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = true, -- add a border to hover docs and signature help
       },
+      routes = {
+        {
+          -- https://github.com/folke/noice.nvim/issues/1097
+          filter = { event = 'msg_show', kind = { 'shell_out', 'shell_err' } },
+          view = 'split',
+          opts = {
+            level = 'info',
+            skip = false,
+            replace = false,
+          },
+        },
+      },
       notify = {
-        enabled = true,
+        enabled = false,
         view = 'notify',
       },
     },
